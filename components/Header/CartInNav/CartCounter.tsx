cb284c1c3e6fe6414c90c25f0fae2a85d6b1d23e
Dawid2806 <david-kaczmarek@gmx.de>
2022-12-05 20:04:31 +0100

    chore change button to div

--- components/Header/CartInNav/CartCounter.tsx
+++ components/Header/CartInNav/CartCounter.tsx
@@ -8,11 +8,11 @@ export const CartItem = () => {
 
   return (
     <Link  href="/cart">
-    <button type="button"className="relative mr-16 lg:mr-8 rounded-full bg-gray-800 p-1 text-gray-400 ">
+    <div className="relative mr-16 lg:mr-8 rounded-full bg-gray-800 p-1 text-gray-400 ">
         <span className='absolute mx-1 text-white '>{cartState.items.length === 0 ? null : cartState.items.length}</span>
         <span className="sr-only">Cart</span>
                 <ShoppingCartIcon className="h-6 w-6 mr-10" aria-hidden="true"/>
-    </button>
+      </div>
     </Link>
   )
 }
